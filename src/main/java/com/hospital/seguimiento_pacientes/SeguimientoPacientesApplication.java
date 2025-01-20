01869bec368e107617f55cef99da2aba54f60f6d
gjiroto <kgonzaso@emeal.nttdata.com>
2025-01-20 11:48:29 -0500

    first dev commit

---
+++ src/main/java/com/hospital/seguimiento_pacientes/SeguimientoPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.seguimiento_pacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientoPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguimientoPacientesApplication.class, args);
+	}
+
+}
